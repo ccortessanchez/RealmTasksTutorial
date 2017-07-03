d93db82a2db489dc8925d0ff8587615ee678413d
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-07-03 12:56:55 +0200

    Added task manually

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -28,14 +28,30 @@ final class Task: Object {
 
 class ViewController: UITableViewController {
     
+    var items = List<Task>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        items.append(Task(value: ["text":"My first task"]))
     }
     
     func setupUI() {
         title = "My Tasks"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    //MARK: UITableView
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5:1
+        return cell
     }
 
 }
